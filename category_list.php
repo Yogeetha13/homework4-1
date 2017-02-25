b6b774b7acbd7055c312c470469640fb25606f8f
yogeetha susparsha <ys467@afsconnect1.njit.edu>
2017-02-25 17:52:49 -0500

    updated databases

--- category_list.php
+++ category_list.php
@@ -2,7 +2,7 @@
 require_once('database.php');
 
 // Get all categories
-$query = 'SELECT * FROM categories
+$query = 'SELECT * FROM categories_guitar2
                        ORDER BY categoryID';
 $statement = $db->prepare($query);
 $statement->execute();
--- database.php
+++ database.php
@@ -1,7 +1,8 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+    $dsn = 'mysql:host=sql1.njit.edu;dbname=ys467';
+    $username = 'ys467';
+    
+    $password = '87RTiWqEE';
 
     try {
         $db = new PDO($dsn, $username, $password);

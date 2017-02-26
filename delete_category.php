bd54777ce28ff7f652cb5e429f46dd8f84e59759
yogeetha susparsha <ys467@afsconnect1.njit.edu>
2017-02-26 18:47:11 -0500

    delete category

---
+++ delete_category.php
@@ -0,0 +1,14 @@
+<?php
+require_once('database.php');
+$category_id = filter_input(INPUT_POST, 'category_id',FILTER_VALIDATE_INT);
+if($category_id != false)
+{
+$query = 'DELETE FROM categories_guitar2 WHERE categoryID = :category_id';
+$statement = $db->prepare($query);
+$statement->bindValue(':category_id', $category_id);
+$success = $statement->execute();
+$statement->closeCursor();
+}
+include('category_list.php');
+?>
+
